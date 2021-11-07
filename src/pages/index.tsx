fda308149e05a20fd2ea5d27ed7463b81792b52e
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-11-07 23:01:17 +0900

    sample login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
 import { Box } from '@chakra-ui/react';
 import { LoginButton } from '@/components/LoginButton';
 import { Layout } from '@/components/Layout';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export default function Home() {
+  const [session, loading] = useSession();
+
   return (
     <Layout>
-      <Box textAlign='center'>
-        <LoginButton />
-      </Box>
+      {!session && (
+        <>
+          {loading ? (
+            <>Loading ...</>
+          ) : (
+            <>
+              Not signed in <br />
+              <button onClick={() => signIn()}>Sign in</button>
+            </>
+          )}
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as <img src={session.user.image ?? ''} width='50px' />
+          {session.user.name} <br />
+          AccessToken : {session.accessToken} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
     </Layout>
   );
 }

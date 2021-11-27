a1d51adabd3f86c22abd7b40d4880f978ccecadd
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-11-28 00:02:49 +0900

    style: ユーザーカードの画像を削除

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,20 +1,16 @@
-import { Box, Button, HStack, VStack, Center } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import { memo } from 'react';
-import { FaTwitter } from 'react-icons/fa';
 import { User } from 'type';
 const UserCard: React.VFC<User> = (props) => {
   const { id, name, username } = props;
   return (
-    <HStack spacing='5' margin='0 auto'>
-      <FaTwitter color='navy' size='30' />
-      <VStack spacing='0.5'>
-        <Text fontWeight='medium' fontSize='large'>
-          {name}
-        </Text>
-        <Text>@{username}</Text>
-      </VStack>
-    </HStack>
+    <VStack spacing='0.5'>
+      <Text fontWeight='medium' fontSize='large'>
+        {name}
+      </Text>
+      <Text>@{username}</Text>
+    </VStack>
   );
 };
 

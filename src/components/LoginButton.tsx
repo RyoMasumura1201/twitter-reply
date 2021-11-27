553d23fdb3fe384326b9c9bbe2a7c887508d5aa6
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-11-27 22:52:33 +0900

    コンポーネントのmemo化

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@chakra-ui/react';
 import { FaTwitter } from 'react-icons/fa';
 import { signIn } from 'next-auth/client';
+import { memo } from 'react';
 
-export const LoginButton: React.VFC = () => {
+const LoginButton: React.VFC = () => {
   return (
     <Button colorScheme='blue' leftIcon={<FaTwitter />} onClick={() => signIn()}>
       ログイン
     </Button>
   );
 };
+
+export default memo(LoginButton);
--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@chakra-ui/react';
 import { FaTwitter } from 'react-icons/fa';
 import { signOut } from 'next-auth/client';
+import { memo } from 'react';
 
-export const LogoutButton: React.VFC = () => {
+const LogoutButton: React.VFC = () => {
   return (
     <Button colorScheme='blue' leftIcon={<FaTwitter />} onClick={() => signOut()}>
       ログアウト
     </Button>
   );
 };
+
+export default memo(LogoutButton);

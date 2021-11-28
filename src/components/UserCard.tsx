7b77de2cc5446123338f5f7e3e6ef89d17276781
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-11-28 23:28:47 +0900

    Usercardからいいね検索apiを叩けるように

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,10 +2,27 @@ import { VStack } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import { memo } from 'react';
 import { User } from 'type';
+import axios from 'axios';
 const UserCard: React.VFC<User> = (props) => {
   const { id, name, username } = props;
+  const handleSearchTweets = () => {
+    axios
+      .get(`/api/twitter/likedTweets/${id}`)
+      .then((res) => {
+        console.log('成功した');
+      })
+      .catch((e) => {
+        console.log('失敗した');
+        console.log(e);
+      });
+  };
   return (
-    <VStack spacing='0.5'>
+    <VStack
+      spacing='0.5'
+      onClick={handleSearchTweets}
+      style={{ cursor: 'pointer' }}
+      className='hover'
+    >
       <Text fontWeight='medium' fontSize='large'>
         {name}
       </Text>

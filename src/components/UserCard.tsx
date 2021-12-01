7c10bb3964c297586838c58ef973b9ef3fa06b3f
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2021-12-02 07:13:48 +0900

    spinner追加

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import { VStack, HStack } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/react';
 import { memo, useState } from 'react';
 import { User } from 'type';
 import axios from 'axios';
@@ -23,26 +24,32 @@ const UserCard: React.VFC<Readonly<User>> = (props) => {
       });
   };
   return (
-    <HStack
-      onClick={handleSearchTweets}
-      style={{ cursor: 'pointer' }}
-      className='hover'
-      spacing='5'
-    >
-      <Image
-        src={profile_image_url}
-        width='50px'
-        height='50px'
-        alt='userimage'
-        className='user-image'
-      />
-      <VStack spacing='0.5'>
-        <Text fontWeight='medium' fontSize='large'>
-          {name}
-        </Text>
-        <Text>@{username}</Text>
-      </VStack>
-    </HStack>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <HStack
+          onClick={handleSearchTweets}
+          style={{ cursor: 'pointer' }}
+          className='hover'
+          spacing='5'
+        >
+          <Image
+            src={profile_image_url}
+            width='50px'
+            height='50px'
+            alt='userimage'
+            className='user-image'
+          />
+          <VStack spacing='0.5'>
+            <Text fontWeight='medium' fontSize='large'>
+              {name}
+            </Text>
+            <Text>@{username}</Text>
+          </VStack>
+        </HStack>
+      )}
+    </>
   );
 };
 
